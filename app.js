2d7f647a0d893203b0709738376e84eab57aba4f
Yasminnairp <yasminnairp@gmail.com>
2025-03-17 13:09:23 -0300

    Primera

---
+++ app.js
@@ -0,0 +1,7 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+//Agrega nombres//
+Function agregarAmigos() {
+
+}
