7554bdf8313fb27bd51f9506d42ccd06aaaa3f7e
Yasminnairp <yasminnairp@gmail.com>
2025-03-17 13:56:59 -0300

    crea el index.html

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-//Agrega nombres//
-Function agregarAmigos() {
-njhbnhjbbjh
-}
-
+ agregarAmigos() 
