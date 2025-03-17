b8a3a8f60634a438e289cc895341a60a4e44b398
Yasminnairp <yasminnairp@gmail.com>
2025-03-17 14:02:35 -0300

    agregar función

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
- agregarAmigos() 
+//Agrega nombres//
+Function agregarAmigos() {
+}
+
