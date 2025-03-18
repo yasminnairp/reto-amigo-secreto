6fab8b72da8e24572c656d0fd23adf79881d4b96
yasminnairp <yasminnairp@gmail.com>
2025-03-18 00:51:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ if (nombreAmigo.trim() === ""){
 
 //Actualiza la lista de amigos
 function mostrarListaAmigo(){
-    let listaAmigos = document.querySelector("listaAmigos");
+    let listaAmigos = document.querySelector("#listaAmigos");
     listaAmigos.innerHTML = "";
     for (let index = 0; index < amigos.length; index++) {
         const element = amigos [index];
