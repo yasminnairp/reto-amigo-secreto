7d6b3d33cc726a626d40f2ed1dc353d8eb8e77e8
Yasminnairp <yasminnairp@gmail.com>
2025-03-18 00:02:13 -0300

    Agrega Amigos

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-//Agrega nombres//
-Function agregarAmigos() {
+//Agrega amigos
+function agregarAmigo () { 
+let nombreAmigo = document.getElementById("amigo").value;
+
+if (nombreAmigo.trim() === ""){
+    alert("Por favor, inserte un nombre");
+}    else {
+    amigos.push(nombreAmigo);
+    document.querySelector("amigo").value = "";
+    mostrarListaAmigo();
+}
 }
 
+//Actualiza la lista de amigos
