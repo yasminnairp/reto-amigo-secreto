65f6286b5751d1d1544384b11352b35b848e5925
Yasminnairp <yasminnairp@gmail.com>
2025-03-18 01:16:21 -0300

    Sortea los amigos

--- app.js
+++ app.js
@@ -16,11 +16,11 @@ if (nombreAmigo.trim() === ""){
 
 //Actualiza la lista de amigos
 function mostrarListaAmigo(){
-    let listaAmigos = document.querySelector("listaAmigos");
+    let listaAmigos = document.querySelector("#listaAmigos");
     listaAmigos.innerHTML = "";
     for (let index = 0; index < amigos.length; index++) {
         const element = amigos [index];
-        
+
         let listaHTML = document.createElement("li");
         listaHTML.textContent =element;
         listaAmigos.appendChild(listaHTML);
