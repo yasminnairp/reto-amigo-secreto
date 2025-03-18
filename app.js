2e495e2a31de95cbfc0638bda19f7721dfb89c52
Yasminnairp <yasminnairp@gmail.com>
2025-03-18 00:43:11 -0300

    Actualiza

--- app.js
+++ app.js
@@ -7,19 +7,20 @@ let nombreAmigo = document.getElementById("amigo").value;
 
 if (nombreAmigo.trim() === ""){
     alert("Por favor, inserte un nombre");
-}    else {
+}  else {
     amigos.push(nombreAmigo);
-    document.querySelector("amigo").value = "";
+    document.querySelector("#amigo").value = "";
     mostrarListaAmigo();
-}
+} 
 }
 
 //Actualiza la lista de amigos
 function mostrarListaAmigo(){
     let listaAmigos = document.querySelector("listaAmigos");
     listaAmigos.innerHTML = "";
-    for (let index = 0; index < amigos.length; index++){
+    for (let index = 0; index < amigos.length; index++) {
         const element = amigos [index];
+        
         let listaHTML = document.createElement("li");
         listaHTML.textContent =element;
         listaAmigos.appendChild(listaHTML);
